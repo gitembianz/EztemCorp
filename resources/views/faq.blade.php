5f9154ff7e30a8494fc292630dbfafdbbc3dafe4
Stanislav Cortac <stanislav.cortac@eztemcorp.com>
2023-09-08 09:24:05 +0300

    some fixing and cleaning

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -2,33 +2,33 @@
 <main>
     <div class="container faq__container">
         <div class="faq__text">
-            <h1 data-text="faq_heading">
+            <h1>
                 @lang("faq.heading")
             </h1>
-            <p data-text="faq_intro_paragraph">
+            <p>
                 @lang("faq.description1")
             </p>
-            <p data-text="faq_experience_paragraph">
+            <p>
                 @lang("faq.description2")
             </p>
-            <p data-text="faq_browse_paragraph">
+            <p>
                 @lang("faq.description3")
             </p>
-            <p data-text="faq_purpose_paragraph">
+            <p>
                 @lang("faq.description4")
             </p>
-            <h3 data-text="faq_get_started_paragraph">
+            <h3>
                 @lang("faq.description5")
             </h3>
         </div>
         <div class="faq__accordions">
             <div class="accordion">
-                <button class="accordion-button" data-text="faq_question_1">
+                <button class="accordion-button">
                     @lang("faq.accordion_list.accordion_1.question")
                 </button>
                 <div class="accordion-wrapper">
                     <div class="accordion-content">
-                        <p data-text="faq_answer_1">
+                        <p>
                             @lang("faq.accordion_list.accordion_1.answer")
                         </p>
                     </div>
@@ -36,12 +36,12 @@
             </div>
 
             <div class="accordion">
-                <button class="accordion-button" data-text="faq_question_2">
+                <button class="accordion-button">
                     @lang("faq.accordion_list.accordion_2.question")
                 </button>
                 <div class="accordion-wrapper">
                     <div class="accordion-content">
-                        <p class="accordion-content" data-text="faq_answer_2">
+                        <p class="accordion-content">
                             @lang("faq.accordion_list.accordion_2.answer")
                         </p>
                     </div>
@@ -49,12 +49,12 @@
             </div>
 
             <div class="accordion">
-                <button class="accordion-button" data-text="faq_question_3">
+                <button class="accordion-button">
                     @lang("faq.accordion_list.accordion_3.question")
                 </button>
                 <div class="accordion-wrapper">
                     <div class="accordion-content">
-                        <p data-text="faq_answer_3">
+                        <p>
                             @lang("faq.accordion_list.accordion_3.answer")
                         </p>
                     </div>
@@ -62,12 +62,12 @@
             </div>
 
             <div class="accordion">
-                <button class="accordion-button" data-text="faq_question_4">
+                <button class="accordion-button">
                     @lang("faq.accordion_list.accordion_4.question")
                 </button>
                 <div class="accordion-wrapper">
                     <div class="accordion-content">
-                        <p data-text="faq_answer_4">
+                        <p>
                             @lang("faq.accordion_list.accordion_4.answer")
                         </p>
                     </div>
@@ -75,12 +75,12 @@
             </div>
 
             <div class="accordion">
-                <button class="accordion-button" data-text="faq_question_5">
+                <button class="accordion-button">
                     @lang("faq.accordion_list.accordion_5.question")
                 </button>
                 <div class="accordion-wrapper">
                     <div class="accordion-content">
-                        <p data-text="faq_answer_5">
+                        <p>
                             @lang("faq.accordion_list.accordion_5.answer")
                         </p>
                     </div>
@@ -88,12 +88,12 @@
             </div>
 
             <div class="accordion">
-                <button class="accordion-button" data-text="faq_question_6">
+                <button class="accordion-button">
                     @lang("faq.accordion_list.accordion_6.question")
                 </button>
                 <div class="accordion-wrapper">
                     <div class="accordion-content">
-                        <p data-text="faq_answer_6">
+                        <p>
                             @lang("faq.accordion_list.accordion_6.answer")
                         </p>
                     </div>
@@ -101,12 +101,12 @@
             </div>
 
             <div class="accordion">
-                <button class="accordion-button" data-text="faq_question_7">
+                <button class="accordion-button">
                     @lang("faq.accordion_list.accordion_7.question")
                 </button>
                 <div class="accordion-wrapper">
                     <div class="accordion-content">
-                        <p data-text="faq_answer_7">
+                        <p>
                             @lang("faq.accordion_list.accordion_7.answer")
                         </p>
                     </div>
@@ -114,12 +114,12 @@
             </div>
 
             <div class="accordion">
-                <button class="accordion-button" data-text="faq_question_8">
+                <button class="accordion-button">
                     @lang("faq.accordion_list.accordion_8.question")
                 </button>
                 <div class="accordion-wrapper">
                     <div class="accordion-content">
-                        <p data-text="faq_answer_8">
+                        <p>
                             @lang("faq.accordion_list.accordion_8.answer")
                         </p>
                     </div>
@@ -127,12 +127,12 @@
             </div>
 
             <div class="accordion">
-                <button class="accordion-button" data-text="faq_question_9">
+                <button class="accordion-button">
                     @lang("faq.accordion_list.accordion_9.question")
                 </button>
                 <div class="accordion-wrapper">
                     <div class="accordion-content">
-                        <p data-text="faq_question_9">
+                        <p>
                             @lang("faq.accordion_list.accordion_9.answer")
                         </p>
                     </div>
@@ -140,12 +140,12 @@
             </div>
 
             <div class="accordion">
-                <button class="accordion-button" data-text="faq_question_10">
+                <button class="accordion-button">
                     @lang("faq.accordion_list.accordion_10.question")
                 </button>
                 <div class="accordion-wrapper">
                     <div class="accordion-content">
-                        <p data-text="faq_answer_10">
+                        <p>
                             @lang("faq.accordion_list.accordion_10.answer")
                         </p>
                     </div>
