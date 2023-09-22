93fa4d3e89a4aa61cc9544ec2cfc8427bb15f672
Stanislav Cortac <stanislav.cortac@eztemcorp.com>
2023-09-22 11:40:36 +0300

    fixed gaps

--- resources/views/retouch.blade.php
+++ resources/views/retouch.blade.php
@@ -26,88 +26,90 @@
             <h3>
                 @lang("retouch.description2")
             </h3>
-            <article class="product__item">
-                <img src="/img/illustrations/photo-correction.webp" alt="front-end">
-                <div class="product__item--text">
-                    <ul>
-                        <li>
-                            <h3>
-                                @lang("retouch.product_list.item1.title")
-                            </h3>
-                        </li>
-                        <li>
-                            <p>
-                                <strong>
-                                    @lang("retouch.product_list.item1.description1.bold")
-                                </strong>
-                                @lang("retouch.product_list.item1.description1.text")
-                            </p>
-                        </li>
-                        <li>
-                            <p>
-                                <strong>
-                                    @lang("retouch.product_list.item1.description2.bold")
-                                </strong>
-                                @lang("retouch.product_list.item1.description2.text")
-                            </p>
-                        </li>
-                        <li>
-                            <p>
-                                <strong>
-                                    @lang("retouch.product_list.item1.description3.bold")
-                                </strong>
-                                @lang("retouch.product_list.item1.description3.text")
-                            </p>
-                        </li>
-                        <li>
-                            <a href="/retouch/photo" class="button">
-                                @lang("retouch.product_list.button")
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </article>
-            <article class="product__item">
-                <img src="/img/illustrations/photo-manipulation.webp" alt="front-end">
-                <div class="product__item--text">
-                    <ul>
-                        <li>
-                            <h3>
-                                @lang("retouch.product_list.item2.title")
-                            </h3>
-                        </li>
-                        <li>
-                            <p>
-                                <strong>
-                                    @lang("retouch.product_list.item2.description1.bold")
-                                </strong>
-                                @lang("retouch.product_list.item2.description1.text")
-                            </p>
-                        </li>
-                        <li>
-                            <p>
-                                <strong>
-                                    @lang("retouch.product_list.item2.description2.bold")
-                                </strong>
-                                @lang("retouch.product_list.item2.description2.text")
-                            </p>
-                        </li>
-                        <li>
-                            <p>
-                                <strong>
-                                    @lang("retouch.product_list.item2.description3.bold")
-                                </strong>
-                                @lang("retouch.product_list.item2.description3.text")
-                            </p>
-                        </li>
-                        <li>
-                            <a href="/retouch/manipulation" class="button">
-                                @lang("retouch.product_list.button")
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </article>
+            <div class="product__list">
+                <article class="product__item">
+                    <img src="/img/illustrations/photo-correction.webp" alt="front-end">
+                    <div class="product__item--text">
+                        <ul>
+                            <li>
+                                <h3>
+                                    @lang("retouch.product_list.item1.title")
+                                </h3>
+                            </li>
+                            <li>
+                                <p>
+                                    <strong>
+                                        @lang("retouch.product_list.item1.description1.bold")
+                                    </strong>
+                                    @lang("retouch.product_list.item1.description1.text")
+                                </p>
+                            </li>
+                            <li>
+                                <p>
+                                    <strong>
+                                        @lang("retouch.product_list.item1.description2.bold")
+                                    </strong>
+                                    @lang("retouch.product_list.item1.description2.text")
+                                </p>
+                            </li>
+                            <li>
+                                <p>
+                                    <strong>
+                                        @lang("retouch.product_list.item1.description3.bold")
+                                    </strong>
+                                    @lang("retouch.product_list.item1.description3.text")
+                                </p>
+                            </li>
+                            <li>
+                                <a href="/retouch/photo" class="button">
+                                    @lang("retouch.product_list.button")
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </article>
+                <article class="product__item">
+                    <img src="/img/illustrations/photo-manipulation.webp" alt="front-end">
+                    <div class="product__item--text">
+                        <ul>
+                            <li>
+                                <h3>
+                                    @lang("retouch.product_list.item2.title")
+                                </h3>
+                            </li>
+                            <li>
+                                <p>
+                                    <strong>
+                                        @lang("retouch.product_list.item2.description1.bold")
+                                    </strong>
+                                    @lang("retouch.product_list.item2.description1.text")
+                                </p>
+                            </li>
+                            <li>
+                                <p>
+                                    <strong>
+                                        @lang("retouch.product_list.item2.description2.bold")
+                                    </strong>
+                                    @lang("retouch.product_list.item2.description2.text")
+                                </p>
+                            </li>
+                            <li>
+                                <p>
+                                    <strong>
+                                        @lang("retouch.product_list.item2.description3.bold")
+                                    </strong>
+                                    @lang("retouch.product_list.item2.description3.text")
+                                </p>
+                            </li>
+                            <li>
+                                <a href="/retouch/manipulation" class="button">
+                                    @lang("retouch.product_list.button")
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </article>
+            </div>
         </div>
     </div>
 </main>
