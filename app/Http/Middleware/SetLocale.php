9f436840b00939ea77491a23437ec74e3ef8521c
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-10-03 13:53:39 +0300

    update

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Session;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -22,7 +21,6 @@ class SetLocale
         } else {
             App::setLocale('en');
         }
-        URL::defaults(['locale' => 'en']);
         return $next($request);
     }
 }
