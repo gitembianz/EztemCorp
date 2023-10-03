44dc9c8e0b5e3e0ef66d61386dc29122f192e475
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-10-03 10:04:13 +0300

    General-> update locale system

--- app/Http/Controllers/mainController.php
+++ app/Http/Controllers/mainController.php
@@ -8,8 +8,18 @@ class mainController extends Controller
     public function changeLocale($locale)
     {
         session()->put('locale', $locale);
-        $pathWithoutQueryParams = parse_url(url()->previous(), PHP_URL_PATH);
-        $newUrl = url($pathWithoutQueryParams);
+        $previousUrl = url()->previous();
+        $parsedUrl = parse_url($previousUrl);
+        $path = $parsedUrl['path'];
+        $queryParams = $parsedUrl['query'] ?? '';
+        if ($queryParams) {
+            parse_str($queryParams, $queryParamsArray);
+            $queryParamsArray['locale'] = $locale;
+            $newQueryParams = http_build_query($queryParamsArray);
+            $newUrl = $path . '?' . $newQueryParams;
+        } else {
+            $newUrl = $path;
+        }
         return redirect($newUrl);
     }
 }
